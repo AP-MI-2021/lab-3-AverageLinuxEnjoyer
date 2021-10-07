6e85bd515255fbaec24a62b00c7bce7c127fbbb0
nullptr <galanlucian002@gmail.com>
2021-10-07 20:26:39 +0300

    Refacut get_longest_div_k

--- get_longest_div_k.py
+++ get_longest_div_k.py
@@ -1,38 +1,30 @@
 from get_longest_subsequence import get_longest_subsequence
+from typing import Callable
 
-def are_div_by_k(lst: list[int]) -> bool:
-    """Verifica daca numerele dintr-o liste sunt divizibile cu k
+def are_div_by_k_lambda(k: int) -> Callable:
+    """Returneaza o functie lambda care verifica divizibilitatea tuturor elementelor unei liste la un numar dat
 
     Args:
-        lst (list[int]): Lista verificata
+        k (int): Numarul dat mai departe functiei lambda
 
     Returns:
-        bool: True daca toate numerele sunt divizibile cu k, False in caz contrar
+        Callable: Functia lambda returnata
     """
-    for x in lst:
-        if x % are_div_by_k.k != 0:
-            return False
+    
+    return lambda lst: all(x % k == 0 for x in lst)
 
-    return True
+def test_are_div_by_k_lambda():
+    assert are_div_by_k_lambda(6)([36,6,12,48]) == True
 
-def test_are_div_by_k():
-    are_div_by_k.k = 6
-    assert are_div_by_k([36,6,12,48]) == True
+    assert are_div_by_k_lambda(1)([123,346,123,456745]) == True
 
-    are_div_by_k.k = 1
-    assert are_div_by_k([123,346,123,456745]) == True
+    assert are_div_by_k_lambda(23142345)([0]) == True
 
-    are_div_by_k.k = 23142345
-    assert are_div_by_k([0]) == True
+    assert are_div_by_k_lambda(129)([]) == True
 
-    are_div_by_k.k = 129
-    assert are_div_by_k([]) == True
+    assert are_div_by_k_lambda(16)([2,3,12,5,9]) == False
 
-    are_div_by_k.k = 16
-    assert are_div_by_k([2,3,12,5,9]) == False
-
-test_are_div_by_k()
-
+test_are_div_by_k_lambda()
 
 def get_longest_div_k(lst: list[int], k: int) -> list[int]:
     """Returneaza cea mai lunga subsecventa cu numere divizibile cu k dintr-o lista
@@ -44,10 +36,7 @@ def get_longest_div_k(lst: list[int], k: int) -> list[int]:
     Returns:
         list[int]: Subsecventa cu numerele divizibile la k
     """
-    are_div_by_k.k = k
-    return get_longest_subsequence(lst, are_div_by_k)
-
-print()
+    return get_longest_subsequence(lst, are_div_by_k_lambda(k))
 
 def test_get_longest_div_k():
     assert get_longest_div_k([12,45,63,29,56,48,32,78,91,115], 2) == [56,48,32,78]
